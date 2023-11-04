164bdae2f72cde87b12dbf754b218d68532ac4d4
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2023-11-04 12:14:02 +0500

    js ass 12-13

---
+++ app.js
@@ -0,0 +1,69 @@
+// Q1====================
+
+// let user = prompt('enter num ')
+// if(user === "A" || user === "65"){
+//     alert(' uppercase letter, ASCII code for A')
+// }else if(user ===  "Z" ||user === "90"){
+//     alert(" alpahabet Z ,ASCII code for Z")
+// }else if (user === "a"|| user === "97"){
+//     alert('a ,ASCII code for a')
+// }else if (user === "z" || user === "122"){
+//     alert('z ,ASCII code for z')
+// }else {
+//         alert('Input is not recognized as a character or ASCII number.');
+//     }
+
+// Q2===================
+// let num1 = +prompt('Enter anumber')
+// let num2 = +prompt('Enter anumber')
+// if(num1>num2){
+//     alert("num1 is greater than")
+// }else if(num1<num2){
+//     alert("num2 is greater than")
+// }else if (num1 == num2){
+//     alert("Equal")
+// }
+
+
+// Q3=================
+// let user = prompt("enter one alphabet")
+// if(user === "a"|| user === "e"|| user ===  "i" || user ===  "o" || user ===  "u"){
+//     alert('True')
+// }else{
+//     alert("False")
+// }
+
+//  Q4===========
+// let password = "112233"
+// let user = prompt('Please enter your password')
+// if(user === "112233"){
+//     alert('Correct!')
+// }else{
+//     alert('Incorrect password!')
+// }
+// Q5===================
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// alert("Good day");
+// }else{
+// alert("Good evening") ;
+// }
+// Q6===================
+// let userInput = prompt("Enter a time in 24-hour format (e.g., 1900):");
+// let time = parseInt(userInput);
+
+// if (isNaN(time)) {
+//   alert("Invalid input. Please enter a valid time in 24-hour format.");
+// } else if (time >= 0 && time < 1200) {
+//   alert("Good morning!");
+// } else if (time >= 1200 && time < 1700) {
+//   alert("Good afternoon!");
+// } else if (time >= 1700 && time < 2000) {
+//   alert("Good evening!");
+// } else if (time >= 2000 && time <= 2359) {
+//   alert("Good night!");
+// } else {
+//   alert("Invalid input. Please enter a valid time in 24-hour format.");
+// }
+// /Assignment task copmleted ========================
